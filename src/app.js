9a6a604edbfdd76c4e69907ff447308d519f7cef
sahilvaghasiya2003 <sahilvaghasiyaaa@gmail.com>
2025-11-23 16:08:03 +0530

    Utils code discussion error and resp;onse utility

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//use method is use for configure the middleware
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//limit the json data size
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+//configuration to store temp file in public folder
+app.use(express.static("public"))
+
+//to read/write secure cookie
+app.use(cookieParser())
+
+
+export { app };
