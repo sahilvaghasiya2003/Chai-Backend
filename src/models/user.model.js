24e124c5ab1330f525b8af5ef8f69526c347d227
sahilvaghasiya2003 <sahilvaghasiyaaa@gmail.com>
2025-11-27 22:43:04 +0530

    Add user register controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,7 +56,7 @@ userSchema.pre("save", async function (next) {
 
     //If password field is not modified then next
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import { exit } from "process";
+
 
 // Configuration
 cloudinary.config({
@@ -28,14 +28,4 @@ const uploadOnCloudinary = async (localFilePath) => {
   }
 };
 
-// Upload an image
-const uploadResult = await cloudinary.uploader
-  .upload(
-    "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
-    {
-      public_id: "shoes",
-    }
-  )
-  .catch((error) => {
-    console.log(error);
-  });
+export {uploadOnCloudinary}
